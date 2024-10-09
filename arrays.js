0b6bf22208bbf27d9c4d95906fdbadc940ed2970
jo-svg <144480253+jo-svg@users.noreply.github.com>
2024-10-09 12:26:47 -0400

    asdwazd

--- arrays.js
+++
@@ -1,8 +0,0 @@
-const stuff_iWantToDo = ["icey", "eat", "sleep", "yes"]
-stuff_iWantToDo.forEach((act) => { console.log(act)});
-stuff_iWantToDo.filter((act) => {console.log (
-    act.length < 4)
-});
-//if like ts it will print true or false
-const yes = stuff_iWantToDo.filter((act)=> act.length <4);
-console.log(yes);
--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const DOMSelectors = {
     header: document.querySelector("header")
 }
-function changeColor(){
-    let button = document.querySelector(".search");
-    console.log(button)
-    button.addEventListener("click", function (event){
+function submit(){
+    let sub_button = document.querySelector(".search");
+    sub_button.addEventListener("click", function (event){
         event.preventDefault(); 
         console.log(event.target);
     });
+    
 }
-changeColor();
+submit();
+
