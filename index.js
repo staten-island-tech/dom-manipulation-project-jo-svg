725adfdd38a20434900db907b8ca34b07804940f
jo-svg <144480253+jo-svg@users.noreply.github.com>
2024-10-10 12:29:52 -0400

    sad

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const DOMSelectors = {
-    header: document.querySelector("header")
+    form: document.querySelector("form"),
+    header: document.querySelector(".header"),
+    container: document.querySelector(".container"),
+    card: document.querySelector(".card"), 
+    bg_color: document.querySelector(".bg-color"),
+    url: document.querySelector(".url"),
+
 }
-function submit(){
+/* function submit(){
     let sub_button = document.querySelector(".search");
     sub_button.addEventListener("click", function (event){
         event.preventDefault(); 
         console.log(event.target);
     });
-    
+} */
+function values(){
+    DOMSelectors.form.addEventListener("submit", function(event){
+        console.log("submit");
+        event.preventDefault(); 
+        let inputs = [
+            DOMSelectors.header.value,DOMSelectors.bg_color.value,DOMSelectors.url.value
+        ]
+        console.log(inputs)
+    })
 }
+values()
 submit();
+
 
